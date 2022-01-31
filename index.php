f4d83fdae760829aa1e482532b5e924b1a0fd887
acherigui1 <72609768+acherigui1@users.noreply.github.com>
2022-01-31 19:44:14 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>page HTML</title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8">
-</head>
-<body>
-<h1><?php echo 'TP GIT'?></h1>
-</body>
-</html>
